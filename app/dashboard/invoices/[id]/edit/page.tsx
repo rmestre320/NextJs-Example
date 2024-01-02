7d5ef4f72d347e29452634cc775ceecaac1461f0
Ronald Mestre <ronaldfelipem@gmail.com>
2024-01-02 15:50:21 -0500

    update on title

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,9 +4,11 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
+//Page title
 export const metadata: Metadata = {
   title: 'Edit Invoice',
 };
+
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
