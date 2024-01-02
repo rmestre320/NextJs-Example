c00064dbd649a7455e5c516e62c07bee2bd72707
Ronald Mestre <ronaldfelipem@gmail.com>
2024-01-01 22:47:27 -0500

    Authentication Chap 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,6 +7,9 @@ import { z } from 'zod'; //Validates format
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation'; //Redirects to another path
+//Auth
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -130,5 +133,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
